3ab05f165e5c663d602005bb34720ad2e27d9080
080852 <138621328+080852@users.noreply.github.com>
2024-07-21 21:48:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+// var a = 12;
+
+// module.exports = a;
+
+// var oneLinerJoke = require('one-liner-joke');
+
+// console.log(oneLinerJoke.getRandomJoke());
+
+// var figlet = require("figlet");
+
+// figlet("sudhanshu thik hogya", function (err, data) {
+//     if (err) {
+//       console.log("Something went wrong...");
+//       console.dir(err);
+//       return;
+//     }
+//     console.log(data);
+//   });
+
+// const express = require('express')
+// const app = express()
+
+// app.use(function(req,res,next){
+//   console.log("hello from middleware");
+//   next()
+// });
+
+// app.get('/', function (req, res) {
+//   res.send('SWAGAT KRO HMARA')
+// });
+
+// app.get("/profile", function (req, res) {
+//   res.send('aagya mein swagat kro hmara')
+// });
+
+// app.listen(3000,()=>{
+//     console.log('server run at port 4000')
+
+// })
+
+// const express = require('express');
+// const app = express();
+// const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://127.0.0.1:27017/Mcs-auth')
+// .then(()=>(console.log("DB connected!")))
+
+   
+
+
